f39853c87b00a8c4392f714263a20a495574229f
马德华 <1073969437@qq.com>
2024-02-23 14:54:59 +0800

    menu菜单demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,12 @@
 <template>
-	<view class="content">
-		<nut-button type="primary" @click="datepicker">datepciker示例</nut-button>
+	<view style="margin-top: 10rpx"><nut-button type="primary" @click="datepicker">datepciker示例</nut-button></view>
+
+	<view style="margin-top: 10rpx">
+		<nut-button type="primary" @click="dialog">Dialog对话框示例</nut-button>
+	</view>
+
+	<view style="margin-top: 10rpx">
+		<nut-button type="primary" @click="menu">Menu菜单示例</nut-button>
 	</view>
 </template>
 
@@ -8,6 +14,16 @@
 const datepicker = function () {
 	uni.navigateTo({
 		url: '/pages/nutui/datepicker/datepicker'
+	});
+};
+const dialog = function () {
+	uni.navigateTo({
+		url: '/pages/nutui/dialog/dialog'
+	});
+};
+const menu = function () {
+	uni.navigateTo({
+		url: '/pages/nutui/menu/menu'
 	});
 };
 </script>
---
+++ src/pages/nutui/dialog/dialog.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<nut-cell title="基础弹框" @click="baseClick"></nut-cell>
+		<nut-dialog title="基础弹框" content="这是基础弹框。" v-model:visible="visible1" @cancel="onCancel" @ok="onOk" />
+	</view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const visible1 = ref(false);
+const baseClick = (): void => {
+	visible1.value = true;
+};
+const onCancel = () => {
+	console.log('event cancel');
+};
+const onOk = () => {
+	console.log('event ok');
+};
+</script>
+
+<style></style>
