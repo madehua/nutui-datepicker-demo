1ec6583d353f8d010229721eabbb40ca0686c174
马德华 <1073969437@qq.com>
2024-03-01 19:15:43 +0800

    avatar示例

--- components.d.ts
+++ components.d.ts
@@ -10,14 +10,5 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     KspCropper: typeof import('./src/components/ksp-cropper/ksp-cropper.vue')['default']
-    NutAvatar: typeof import('nutui-uniapp/components/avatar/avatar.vue')['default']
-    NutButton: typeof import('nutui-uniapp/components/button/button.vue')['default']
-    NutCell: typeof import('nutui-uniapp/components/cell/cell.vue')['default']
-    NutDatePicker: typeof import('nutui-uniapp/components/datepicker/datepicker.vue')['default']
-    NutDialog: typeof import('nutui-uniapp/components/dialog/dialog.vue')['default']
-    NutMenu: typeof import('nutui-uniapp/components/menu/menu.vue')['default']
-    NutMenuItem: typeof import('nutui-uniapp/components/menuitem/menuitem.vue')['default']
-    NutPopup: typeof import('nutui-uniapp/components/popup/popup.vue')['default']
-    NutUploader: typeof import('nutui-uniapp/components/uploader/uploader.vue')['default']
   }
 }
--- src/pages/nutui/avatar/avatar.vue
+++ src/pages/nutui/avatar/avatar.vue
@@ -2,18 +2,18 @@
 	<view>
 		<view>
 			<nut-avatar size="normal" shape="round">
-				<img src="https://t7.baidu.com/it/u=1595072465,3644073269&fm=193&f=GIF" />
+				<image src="https://t7.baidu.com/it/u=1595072465,3644073269&fm=193&f=GIF"></image>
 			</nut-avatar>
 		</view>
 		<view>
 			<nut-avatar size="large">
-				<img src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png" />
+				<image src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png"></image>
 			</nut-avatar>
 			<nut-avatar size="normal">
-				<img src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png" />
+				<image src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png"></image>
 			</nut-avatar>
 			<nut-avatar size="small">
-				<img src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png" />
+				<image src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png"></image>
 			</nut-avatar>
 		</view>
 	</view>
